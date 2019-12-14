4f4e5eac4d22aa2d40889730f7e2cd8424a38321
varsharani <varsharanir123@gmail.com>
2019-12-14 13:54:00 +0530

    done

--- src/com/csi/jpacriateria/Employee.java
+++ src/com/csi/jpacriateria/Employee.java
@@ -1,5 +1,41 @@
 package com.csi.jpacriateria;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
+	@Id
+	@GeneratedValue
+	int empId;
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSalary=" + empSalary + ", getEmpId()="
+				+ getEmpId() + ", getEmpName()=" + getEmpName() + ", getEmpSalary()=" + getEmpSalary() + ", getClass()="
+				+ getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(double empSalary) {
+		this.empSalary = empSalary;
+	}
+	String empName;
+	double empSalary;
+	
+	
 
 }
--- src/com/csi/jpacriateria/Services.java
+++ src/com/csi/jpacriateria/Services.java
@@ -1,9 +1,29 @@
 package com.csi.jpacriateria;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+
 public class Services {
+	static SessionFactory factory;
 
 	public static void main(String[] args) {
+		factory = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+		Employee e1 = new Employee();
+		e1.setEmpName("Varsha");
+		e1.setEmpSalary(45000.00);
 
+		session.save(e1);
+		List list=session.createQuery("from Employee").list();
+		list.forEach(a->System.out.println(list));
+		transaction.commit();
+		
+		
 	}
 
 }
